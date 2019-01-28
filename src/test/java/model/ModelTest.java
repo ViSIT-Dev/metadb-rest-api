c872b054699ea97cfb6e889c96d0a00ea2eea4e3
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2019-01-28 14:51:54 +0100

    Added a test for the Group VISMO entity (used for test purposes)

--- src/test/java/model/ModelTest.java
+++ src/test/java/model/ModelTest.java
@@ -1,7 +1,11 @@
 package model;
 
 import com.github.anno4j.Anno4j;
+import com.github.anno4j.querying.QueryService;
 import model.technicalMetadata.DigitalRepresentation;
+import model.vismo.Group;
+import model.vismo.Reference;
+import model.vismo.ReferenceEntry;
 import org.apache.marmotta.ldpath.parser.ParseException;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,15 +15,56 @@ import org.openrdf.repository.RepositoryException;
 
 import java.util.List;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 public class ModelTest {
 
     private Anno4j anno4j;
 
+    private final static String KEYWORD = "keyword1";
+    private final static String KEYWORD2 = "KEYWORD2";
+    private final static String ICONOGRAPHY = "iconography";
+
     @Before
     public void setUp() throws Exception {
         this.anno4j = new Anno4j();
+    }
+
+    @Test
+    public void testGroupAndReferences() throws RepositoryException, IllegalAccessException, InstantiationException, ParseException, MalformedQueryException, QueryEvaluationException {
+        Group group = this.anno4j.createObject(Group.class);
+
+        DigitalRepresentation digrep = this.anno4j.createObject(DigitalRepresentation.class);
+
+        group.addKeyword(KEYWORD);
+        group.addKeyword(KEYWORD2);
+        group.setIconography(ICONOGRAPHY);
+        group.addDigitalRepresentation(digrep);
+
+        ReferenceEntry entry = this.anno4j.createObject(ReferenceEntry.class);
+
+        group.setEntry(entry);
+
+        Reference reference = this.anno4j.createObject(Reference.class);
+
+        entry.setEntryIn(reference);
+
+        QueryService queryService = this.anno4j.createQueryService();
+        queryService.addCriteria(".", group.getResourceAsString());
+
+        List<Group> result = queryService.execute(Group.class);
+
+        assertEquals(1, result.size());
+
+        Group resultGroup = result.get(0);
+
+        assertEquals(entry.getResourceAsString(), resultGroup.getEntry().getResourceAsString());
+        assertEquals(ICONOGRAPHY, resultGroup.getIconography());
+        assertTrue(group.getKeywords().contains(KEYWORD));
+        assertTrue(group.getKeywords().contains(KEYWORD2));
+        assertEquals(1, group.getDigitalRepresentations().size());
+        assertTrue(group.getDigitalRepresentations().contains(digrep));
     }
 
     @Test
