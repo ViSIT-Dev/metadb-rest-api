853b7693ea1294cbbc51467a22fd7d665fe413bb
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2019-02-19 10:54:01 +0100

    Update python script; small error (rdf:type was missing for 5-step props)

--- src/main/java/rest/persistence/util/TripleMerger.java
+++ src/main/java/rest/persistence/util/TripleMerger.java
@@ -1,7 +1,105 @@
 package rest.persistence.util;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.*;
+
 /**
- * Created by Manu on 19.02.19.
+ * Class that supports a method to merge several given RDF triples within their common parts.
  */
 public class TripleMerger {
+    
+    public static String mergeTriples(List<String> wrapperQueries) {
+
+        LinkedList<String> singleTriples = extractSingleTriples(wrapperQueries);
+
+        // Used to remember variable substitutions
+        HashMap<String, String> substitutions = new HashMap<String, String>();
+
+        LinkedList<String> mergedTriples = new LinkedList<String>();
+
+        for(int i = 0; i < singleTriples.size(); ++i) {
+            for(int j = i + 1; j < singleTriples.size(); ++j) {
+                LinkedList<String> intermediateTriples = mergeTwoTriples(singleTriples.get(i), singleTriples.get(j), substitutions);
+
+                for(String triple : intermediateTriples) {
+                    if(!mergedTriples.contains(triple)) {
+                        mergedTriples.add(triple);
+                    }
+                }
+            }
+        }
+
+        return createCombinedTriples(mergedTriples);
+    }
+
+    private static String createCombinedTriples(LinkedList<String> mergedTriples) {
+        String result = "";
+
+        for(String triple : mergedTriples) {
+            result += triple + " .\n";
+        }
+
+        return result;
+    }
+
+    private static LinkedList<String> mergeTwoTriples(String triple1, String triple2, HashMap<String, String> substitutions) {
+
+        LinkedList<String> result = new LinkedList<String>();
+
+        // Check substitutions and do them if found
+        for(String subKey : substitutions.keySet()) {
+            if(triple1.contains(subKey)) {
+                triple1 = triple1.replace(subKey, substitutions.get(subKey));
+            }
+
+            if(triple2.contains(subKey)) {
+                triple2 = triple2.replace(subKey, substitutions.get(subKey));
+            }
+        }
+
+        // Check if the same triples are given
+        if(triple1.equals(triple2)) {
+            result.addAll(Arrays.asList(triple1, triple2));
+        }
+
+        // Do process:
+        String[] triple1Split = triple1.split(" ");
+        String[] triple2Split = triple2.split(" ");
+
+        if(triple1Split[0].equals(triple2Split[0]) && triple1Split[1].equals(triple2Split[1]) && !triple1Split[2].equals(triple2Split[2])) {
+            // Subject and predicate equal, object differing
+            substitutions.put(triple2Split[2], triple1Split[2]);
+
+            result.add(triple1);
+        } else if(!triple1Split[0].equals(triple2Split[0]) && triple1Split[1].equals(triple2Split[1]) && triple1Split[2].equals(triple2Split[2])) {
+            // Predicate and object equal, subject differing
+            substitutions.put(triple2Split[0], triple1Split[0]);
+
+            result.add(triple1);
+        } else {
+            if(!result.contains(triple1)) {
+                result.add(triple1);
+            }
+
+            if(!result.contains(triple2)) {
+                result.add(triple2);
+            }
+        }
+
+        return result;
+    }
+
+    private static LinkedList<String> extractSingleTriples(List<String> wrapperQueries) {
+        LinkedList<String> singleTriples = new LinkedList<String>();
+
+        for(String wrapperQuery : wrapperQueries) {
+            String[] split = wrapperQuery.split(" . ");
+
+            Collections.addAll(singleTriples, split);
+        }
+
+        return singleTriples;
+    }
 }
--- src/test/java/rest/persistence/util/TripleMergerTest.java
+++ src/test/java/rest/persistence/util/TripleMergerTest.java
@@ -1,10 +1,54 @@
 package rest.persistence.util;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import static org.junit.Assert.*;
 
 /**
- * Created by Manu on 19.02.19.
+ * Test-suite for the TripleMerger Class.
  */
 public class TripleMergerTest {
 
+    private final static String TRIPLES1 = "?x <http://erlangen-crm.org/170309/P128_carries> ?y1 . ?y1 rdf:type <http://erlangen-crm.org/170309/E34_Inscription> . ?y1 <http://erlangen-crm.org/170309/P3_has_note> ?inscription_text";
+    private final static String TRIPLES2 = "?x <http://erlangen-crm.org/170309/P128_carries> ?y2 . ?y2 rdf:type <http://erlangen-crm.org/170309/E34_Inscription> . ?y2 <http://erlangen-crm.org/170309/P2_has_type> ?y3 . ?y3 rdf:type <http://erlangen-crm.org/170309/E55_Type> . ?y3 <http://erlangen-crm.org/170309/P3_has_note> ?inscription_has_type";
+    private final static String TRIPLES3 = "?x <http://erlangen-crm.org/170309/P128_carries> ?y4 . ?y4 rdf:type <http://erlangen-crm.org/170309/E34_Inscription> . ?y4 <http://erlangen-crm.org/170309/P106_is_composed_of> ?y5 . ?y5 rdf:type <http://erlangen-crm.org/170309/E37_Mark> . ?y5 <http://erlangen-crm.org/170309/P3_has_note> ?inscription_signature";
+
+    @Test
+    public void testTripleMergerWithTwoTriples() {
+        String result = TripleMerger.mergeTriples(new LinkedList<>(Arrays.asList(TRIPLES1, TRIPLES2)));
+
+        assertTrue(result.contains("?x <http://erlangen-crm.org/170309/P128_carries> ?y1"));
+        assertTrue(result.contains("?y1 rdf:type <http://erlangen-crm.org/170309/E34_Inscription>"));
+        assertTrue(result.contains("?y1 <http://erlangen-crm.org/170309/P3_has_note> ?inscription_text"));
+        assertTrue(result.contains("?y1 <http://erlangen-crm.org/170309/P2_has_type> ?y3"));
+        assertTrue(result.contains("?y3 <http://erlangen-crm.org/170309/P3_has_note> ?inscription_has_type"));
+
+        assertFalse(result.contains("?x <http://erlangen-crm.org/170309/P128_carries> ?y2"));
+        assertFalse(result.contains("?y2 rdf:type <http://erlangen-crm.org/170309/E34_Inscription>"));
+
+        System.out.println("The result is: " + result);
+    }
+
+    @Test
+    public void testTripleMergerWithThreeTriples() {
+        String result = TripleMerger.mergeTriples(new LinkedList<>(Arrays.asList(TRIPLES1, TRIPLES2, TRIPLES3)));
+
+        assertTrue(result.contains("?x <http://erlangen-crm.org/170309/P128_carries> ?y1"));
+        assertTrue(result.contains("?y1 rdf:type <http://erlangen-crm.org/170309/E34_Inscription>"));
+        assertTrue(result.contains("?y1 <http://erlangen-crm.org/170309/P3_has_note> ?inscription_text"));
+        assertTrue(result.contains("?y1 <http://erlangen-crm.org/170309/P2_has_type> ?y3"));
+        assertTrue(result.contains("?y3 <http://erlangen-crm.org/170309/P3_has_note> ?inscription_has_type"));
+        assertTrue(result.contains("?y1 <http://erlangen-crm.org/170309/P106_is_composed_of> ?y5"));
+        assertTrue(result.contains("?y5 <http://erlangen-crm.org/170309/P3_has_note> ?inscription_signature"));
+
+        assertFalse(result.contains("?x <http://erlangen-crm.org/170309/P128_carries> ?y2"));
+        assertFalse(result.contains("?y2 rdf:type <http://erlangen-crm.org/170309/E34_Inscription>"));
+        assertFalse(result.contains("?y4 rdf:type <http://erlangen-crm.org/170309/E34_Inscription>"));
+        assertFalse(result.contains("?y4 <http://erlangen-crm.org/170309/P106_is_composed_of> ?y5"));
+
+        System.out.println("The result is: " + result);
+    }
 }
