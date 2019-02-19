3c2c96eac4d827af8da433f160b2cbf9f01abc63
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2019-02-19 10:53:34 +0100

    Implemented TripleMerger

---
+++ src/main/java/rest/persistence/util/TripleMerger.java
@@ -0,0 +1,7 @@
+package rest.persistence.util;
+
+/**
+ * Created by Manu on 19.02.19.
+ */
+public class TripleMerger {
+}
---
+++ src/test/java/rest/persistence/util/TripleMergerTest.java
@@ -0,0 +1,10 @@
+package rest.persistence.util;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Manu on 19.02.19.
+ */
+public class TripleMergerTest {
+
+}
