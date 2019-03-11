61530e0d80569bb032ffee7a2000c71316e3e0d1
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2019-03-11 15:43:00 +0100

    Initial commit for feature/excel

---
+++ src/main/java/util/excel/ExcelTemplateProducer.java
@@ -0,0 +1,10 @@
+package util.excel;
+
+/**
+ * This Class comprises the functionality to generate a Excel Sheet, which can be used as input for the import
+ * functionality of the Visit REST API.
+ */
+public class ExcelTemplateProducer {
+
+
+}
