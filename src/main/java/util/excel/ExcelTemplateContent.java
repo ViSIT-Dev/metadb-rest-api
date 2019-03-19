4137e3442d69239e900e82b536b0edeec2e06df6
Emanuel Berndl <berndl@dimis.fim.uni-passau.de>
2019-03-19 16:02:55 +0100

    Added remaining contents; Added Colorcoding; Added Freeze pane for top row

--- src/main/java/util/excel/ExcelTemplateContent.java
+++ src/main/java/util/excel/ExcelTemplateContent.java
@@ -17,7 +17,7 @@ public class ExcelTemplateContent {
             {"Activity"},
             {"Titel(String)"},
             {"Beschreibung(String)"},
-            {"Datierung", "Taggenaue Datierung(Date)", "Freie Datierung(String)", "Beginn des Datierungszeitraums(String)","Ende des Datierungszeitraums(String)"},
+            {"Datierung", "Taggenaue Datierung(String)", "Freie Datierung(String)", "Beginn des Datierungszeitraums(String)","Ende des Datierungszeitraums(String)"},
             {"Ort(Place)"},
             {"Beteiligte Personen(Person)"},
             {"Beteiligte Bauwerke(Architecture)"},
@@ -60,8 +60,8 @@ public class ExcelTemplateContent {
             {"Geschichte(String)"},
             {"Ereignis(Activity)"},
             {"Baugeschichte(String)"},
-            {"Bau", "Datierung[5]", "Entstehung:Beginn des Datierungszeitraums(String)", "Entstehung: Ende des Datierungszeitraums(String)", "Entstehung: Freie Datierung(String)", "Entstehung: Jahrhundert(String)", "Auftraggeber Person(Person)", "Ausführende Personen(Person)", "Beteiligte Personen(Person)", "Auftraggeber Gruppe(Group)", "Ausführende Gruppe(Group)", "Beteiligte Gruppe(Group)"},//TODO Geschachtelte Untergruppe
-            {"Bauphasen", "blub(String)"},// TODO
+            {"Bau", "Datierung[5]", "Entstehung:Beginn des Datierungszeitraums(String)", "Entstehung: Ende des Datierungszeitraums(String)", "Entstehung: Freie Datierung(String)", "Entstehung: Jahrhundert(String)", "Auftraggeber Person(Person)", "Ausführende Personen(Person)", "Beteiligte Personen(Person)", "Auftraggeber Gruppe(Group)", "Ausführende Gruppe(Group)", "Beteiligte Gruppe(Group)"},
+            {"Bauphasen", "Titel(String)", "Datierung[5]", "Beginn der Bauphase(String)", "Ende der Bauphase(String)", "Freie Datierung(String)", "Jahrhundert(String)", "Beschreibung(String)", "Funktion(String)", "Auftraggeber Person(Person)", "Ausführende Personen(Person)", "Beteiligte Personen(Person)", "Auftraggeber Gruppe(Group)", "Ausführende Gruppe(Group)", "Beteiligte Gruppe(Group)", "Kommentar(String)"},
             {"Enthaltene Bauteile(Architecture)"},
             {"Bauteil von(Architecture)"},
             {"Eigentümer/Verwalter Person(Person)"},
@@ -92,19 +92,89 @@ public class ExcelTemplateContent {
     };
 
     private final static String[][] OBJECT_FIELDS = {
-            {"Object"}
+            {"Object"},
+            {"Titel(String)"},
+            {"Objektbezeichnung(String)"},
+            {"Funktion(String)"},
+            {"Allgemeine Objektdatierung", "Freie Datierung(String)", "Beginn des Datierungszeitraums(String)", "Ende des Datierungszeitraums(String)", "Jahrhundert(String)"},
+            {"Objekt besteht aus Teilobjekten(Object)"},
+            {"Teilobjekt von(Object)"},
+            {"Beschreibung(String)"},
+            {"Herstellung", "Künstler(Person)", "Werkstatt(Group)", "Datierung[5]", "Entstehungszeit: Freie Datierung(String)", "Entstehungszeit: Beginn des Datierungszeitraums(String)", "Entstehungszeit: Ende des Datierungszeitraums(String)", "Entstehungszeit: Jahrhundert(String)", "Herstellungsort(Place)", "Material(String)", "Technik(String)"},
+            {"Masse", "Abmessung(String)", "Wert(String)"},
+            {"Inschrift", "Art(String)", "Anbringung(String)", "Datierung(String)", "Signatur(String)", "Text(String)"},
+            {"Institution/Eigentümer/Verwalter(Institution)"},
+            {"Inventarnummer(String)"},
+            {"Standort - Museum(Institution)"},
+            {"Standort - Bauwerk(Architecture)"},
+            {"Provenienz", "Datierung des Besitzerwechsels[6]", "Taggenaue Datierung(String)", "Freie Datierung(String)", "Beginn des Datierungszeitraums(String)", "Ende des Datierungszeitraums(String)", "Jahrhundert(String)", "Eigentümer - Person(Person)", "Eigentümer - Gruppe(Group)", "Vorbesitzer - Person(Person)", "Vorbesitzer - Gruppe(Group)"}, // TODO Taggenau to Date?
+            {"Darstellung(String)"},
+            {"Objektbezug(Activity)"},
+            {"Person(Person)"},
+            {"Abgebildetes Bauwerk(Architecture)"},
+            {"Abgebildeter Ort(Place)"},
+            {"Abgebildete Aktivität(Activity)"},
+            {"Schlagwort(String)"},
+            {"Kommentar(String)"},
+            {"Literaturangaben", "Kurztitel(Reference)", "Seiten(String)"},
+            {"Hilfreiche Links(String)"}
     };
 
     private final static String[][] INSTITUTION_FIELDS = {
-            {"Institution"}
+            {"Institution"},
+            {"Name(String)"},
+            {"Bauwerk(Architecture)"},
+            {"Ort(Place)"},
+            {"Adresse(String)"},
+            {"Ausstellung(Reference)"},
+            {"Katalog(Reference)"},
+            {"Hilfreiche Links(String)"}
     };
 
     private final static String[][] PERSON_FIELDS = {
-            {"Person"}
+            {"Person"},
+            {"Vorname(String)"},
+            {"Nachname(String)"},
+            {"Künstlername(String)"},
+            {"Notname(String)"},
+            {"Bezeichnung(String)"},
+            {"Geburt", "Geburtsdatum[5]", "Taggenaue Datierung(String)", "Freie Datierung(String)", "Beginn des Datierungszeitraums(String)", "Ende des Datierungszeitraums(String)", "Geburtsort(Place)", "Mutter(Person)", "Vater(Person)"},
+            {"Tod", "Sterbedatum[5]", "Taggenaue Datierung(String)", "Freie Datierung(String)", "Beginn des Datierungszeitraums(String)", "Ende des Datierungszeitraums(String)", "Sterbeort(Place)"},
+            {"Amt/Beruf(String)"},
+            {"Titel(String)"},
+            {"Beschreibung(String)"},
+            {"Ehe", "Ehepartner(Person)", "Beginn der Ehe[5]", "Taggenaue Datierung(String)", "Freie Datierung(String)", "Beginn des Datierungszeitraums(String)", "Ende des Datierungszeitraums(String)", "Ende der Ehe[5]", "Taggenaue Datierung(String)", "Freie Datierung(String)", "Beginn des Datierungszeitraums(String)", "Ende des Datierungszeitraums(String)"},
+            {"Kinder(Person)"},
+            {"Verwaltung/Eigentum(Architecture)"},
+            {"Teilnahme(Activity)"},
+            {"In Auftrag gegebene Bauwerke(Architecture)"},
+            {"Ausgeführte Bauwerke(Architecture)"},
+            {"Beteiligt an - Bauwerke(Architecture)"},
+            {"In Auftrag gegebene Baumaßnahmen(String)"},
+            {"Ausgeführte Baumaßnahmen(String)"},
+            {"Beteiligt an - Baumaßnahmen(String)"},
+            {"Im Besitz befindliche Objekte(Object)"},
+            {"Ehemals im Besitz befindliche Objekte(Object)"},
+            {"Schlagwort(String)"},
+            {"Kommentar(String)"},
+            {"Literaturangaben", "Kurztitel(Reference)", "Seiten(String)"},
+            {"Hilfreiche Links(String)"},
+            {"Objektbezug(Object)"}
     };
 
     private final static String[][] PLACE_FIELDS = {
-            {"Place"}
+            {"Place"},
+            {"Ortsname(String)"},
+            {"Beschreibung(String)"},
+            {"Bauwerk(Architecture)"},
+            {"Ereignis(Activity)"},
+            {"Geburtsort von Personen(Person)"},
+            {"Sterbeort von Personen(Person)"},
+            {"Schlagwort(String)"},
+            {"Kommentar(String)"},
+            {"Literaturangaben", "Kurztitel(Reference)", "Seiten(String)"},
+            {"Hilfreiche Links(String)"},
+            {"Abbildung(Object)"}
     };
 
     final static List<String[][]> SHEETS = new LinkedList<String[][]>(Arrays.asList(ACTIVITY_FIELDS, GROUP_FIELDS, ARCHITECTURE_FIELDS, REFERENCE_FIELDS, OBJECT_FIELDS, INSTITUTION_FIELDS, PERSON_FIELDS, PLACE_FIELDS));
--- src/main/java/util/excel/ExcelTemplateProducer.java
+++ src/main/java/util/excel/ExcelTemplateProducer.java
@@ -28,20 +28,24 @@ import java.util.Iterator;
 public class ExcelTemplateProducer {
 
     private static final String EXCEL_FILE_NAME = "templates/visitExcel.xlsx";
-    private static final String JSON_FILE_NAME = "templates/json.txt";
+
+    private static final boolean COLOR_CODED = true;
 
     public static void main(String[] args) {
         XSSFWorkbook workbook = new XSSFWorkbook();
 
         for (String[][] sheetInput : ExcelTemplateContent.SHEETS) {
             XSSFSheet sheet = workbook.createSheet(sheetInput[0][0]);
-            sheet.createFreezePane(1,0,1,0);
+            sheet.createFreezePane(1,1,1,1);
 
             Row idRow = sheet.createRow(0);
 
             CellStyle idRowStyle = workbook.createCellStyle();
             idRowStyle.setFillForegroundColor(IndexedColors.GREY_40_PERCENT.getIndex());
             idRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            idRowStyle.setBorderLeft(BorderStyle.THIN);
+            idRowStyle.setBorderRight(BorderStyle.THIN);
+            idRowStyle.setBorderTop(BorderStyle.THIN);
             idRowStyle.setBorderBottom(BorderStyle.THIN);
 
             XSSFFont idRowFont = workbook.createFont();
@@ -69,7 +73,7 @@ public class ExcelTemplateProducer {
                     if(ExcelTemplateContent.ENTITIES.contains(getDataType(sheetInput[i][0]))) {
                         XSSFDataValidationHelper dvHelper = new XSSFDataValidationHelper(sheet);
 
-                        CellRangeAddressList addressList = new CellRangeAddressList(cell.getAddress().getRow(), cell.getAddress().getRow(), cell.getAddress().getColumn() + 1, cell.getAddress().getColumn() + 5);
+                        CellRangeAddressList addressList = new CellRangeAddressList(cell.getAddress().getRow(), cell.getAddress().getRow(), cell.getAddress().getColumn() + 1, cell.getAddress().getColumn() + 500);
 
                         String constraint = "=" + getDataType(sheetInput[i][0]) + "!$B$1:$XFD$1";
 
@@ -80,7 +84,21 @@ public class ExcelTemplateProducer {
 
                         sheet.addValidationData(validation);
                     }
-//                  Do datatype check  getDataType(sheetInput[i][0])
+
+                    // Colorcoding
+//                    if(COLOR_CODED) {
+//                        CellStyle colorStyle = workbook.createCellStyle();
+//                        colorStyle.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE.getIndex());
+//                        colorStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+//                        colorStyle.setBorderLeft(BorderStyle.THIN);
+//                        colorStyle.setBorderRight(BorderStyle.THIN);
+//                        colorStyle.setBorderTop(BorderStyle.THIN);
+//                        colorStyle.setBorderBottom(BorderStyle.THIN);
+//
+//                        row.setRowStyle(colorStyle);
+//                        cell.setCellStyle(colorStyle);
+//                    }
+
                 } else {
                     // Subgroup
                     // Create "header" for subgroup
@@ -93,7 +111,11 @@ public class ExcelTemplateProducer {
                     CellStyle headerStyle = workbook.createCellStyle();
                     headerStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
                     headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-                    headerStyle.setBorderTop(BorderStyle.THIN);
+                    headerStyle.setBorderTop(BorderStyle.MEDIUM);
+
+                    XSSFFont headerFont = workbook.createFont();
+                    headerFont.setBold(true);
+                    headerStyle.setFont(headerFont);
 
                     headerRow.setRowStyle(headerStyle);
                     headerCell.setCellStyle(headerStyle);
@@ -107,19 +129,88 @@ public class ExcelTemplateProducer {
                         Cell cell = row.createCell(0);
                         cell.setCellValue(stripDataType(sheetInput[i][j]));
 
+                        // Add validation if the cell represents a reference
+                        if(sheetInput[i][j].endsWith(")") && ExcelTemplateContent.ENTITIES.contains(getDataType(sheetInput[i][j]))) {
+                            XSSFDataValidationHelper dvHelper = new XSSFDataValidationHelper(sheet);
+
+                            CellRangeAddressList addressList = new CellRangeAddressList(cell.getAddress().getRow(), cell.getAddress().getRow(), cell.getAddress().getColumn() + 1, cell.getAddress().getColumn() + 500);
+
+                            String constraint = "=" + getDataType(sheetInput[i][j]) + "!$B$1:$XFD$1";
+
+                            XSSFDataValidationConstraint formulaListConstraint = (XSSFDataValidationConstraint)
+                                    dvHelper.createFormulaListConstraint(constraint);
+
+                            XSSFDataValidation validation = (XSSFDataValidation) dvHelper.createValidation(formulaListConstraint, addressList);
+
+                            sheet.addValidationData(validation);
+                        }
+
+                        if(COLOR_CODED) {
+                            CellStyle colorStyle = workbook.createCellStyle();
+
+                            if(subsubgroup > 1) {
+                                colorStyle.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE.getIndex());
+                            } else {
+                                colorStyle.setFillForegroundColor(IndexedColors.LIGHT_TURQUOISE.getIndex());
+                            }
+
+                            colorStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                            colorStyle.setBorderLeft(BorderStyle.THIN);
+                            colorStyle.setBorderRight(BorderStyle.THIN);
+                            colorStyle.setBorderTop(BorderStyle.THIN);
+                            colorStyle.setBorderBottom(BorderStyle.THIN);
+
+                            row.setRowStyle(colorStyle);
+                            cell.setCellStyle(colorStyle);
+                        }
+
                         if(sheetInput[i][j].endsWith("]")) {
                             // Sub-subgroup, start counter for fields associated with the subsubgroup
                             subsubgroup = stripSubSubGroupLength(sheetInput[i][j]);
 
                             CellStyle topGroupStyle = workbook.createCellStyle();
-                            topGroupStyle.setBorderTop(BorderStyle.THIN);
+                            if(COLOR_CODED) {
+                                topGroupStyle.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE.getIndex());
+                                topGroupStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                                topGroupStyle.setBorderLeft(BorderStyle.THIN);
+                                topGroupStyle.setBorderRight(BorderStyle.THIN);
+                                topGroupStyle.setBorderBottom(BorderStyle.THIN);
+                            }
+                            topGroupStyle.setBorderTop(BorderStyle.DASHED);
+
+
+                            XSSFFont topGroupFont = workbook.createFont();
+                            topGroupFont.setBold(true);
+                            topGroupStyle.setFont(topGroupFont);
 
                             row.setRowStyle(topGroupStyle);
                             cell.setCellStyle(topGroupStyle);
-                        } else if (j == sheetInput[i].length - 1 || subsubgroup == 1) {
+                        } else if(subsubgroup == 1) {
+                            CellStyle bottomSubGroupStyle = workbook.createCellStyle();
+                            if(COLOR_CODED) {
+                                bottomSubGroupStyle.setFillForegroundColor(IndexedColors.LIGHT_CORNFLOWER_BLUE.getIndex());
+                                bottomSubGroupStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                                bottomSubGroupStyle.setBorderLeft(BorderStyle.THIN);
+                                bottomSubGroupStyle.setBorderRight(BorderStyle.THIN);
+                                bottomSubGroupStyle.setBorderTop(BorderStyle.THIN);
+                            }
+                            bottomSubGroupStyle.setBorderBottom(BorderStyle.DASHED);
+
+                            row.setRowStyle(bottomSubGroupStyle);
+                            cell.setCellStyle(bottomSubGroupStyle);
+                        } else if (j == sheetInput[i].length - 1) {
                             // End of subgroup reached, so add closing style
                             CellStyle bottomGroupStyle = workbook.createCellStyle();
-                            bottomGroupStyle.setBorderBottom(BorderStyle.THIN);
+
+                            if(COLOR_CODED) {
+                                bottomGroupStyle.setFillForegroundColor(IndexedColors.LIGHT_TURQUOISE.getIndex());
+                                bottomGroupStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+                                bottomGroupStyle.setBorderLeft(BorderStyle.THIN);
+                                bottomGroupStyle.setBorderRight(BorderStyle.THIN);
+                                bottomGroupStyle.setBorderTop(BorderStyle.THIN);
+                            }
+
+                            bottomGroupStyle.setBorderBottom(BorderStyle.MEDIUM);
 
                             row.setRowStyle(bottomGroupStyle);
                             cell.setCellStyle(bottomGroupStyle);
@@ -129,15 +220,12 @@ public class ExcelTemplateProducer {
                             subsubgroup -= 1;
                         }
                     }
-
-
                 }
             }
 
             sheet.autoSizeColumn(0);
 
         }
-
 
         try {
             FileOutputStream os = new FileOutputStream(EXCEL_FILE_NAME);
